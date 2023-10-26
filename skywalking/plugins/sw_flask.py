d51f6613a8fa950c239e11df620ffe0dcd2275c3
JayChow <4192339+westarest@users.noreply.github.com>
2023-10-26 09:57:08 +0800

    fix unit test

--- skywalking/plugins/sw_flask.py
+++ skywalking/plugins/sw_flask.py
@@ -25,7 +25,11 @@ link_vector = ['https://flask.palletsprojects.com']
 support_matrix = {
     'flask': {
         '>=3.7': ['2.0']  # 1.x removed due to EOL
+    },
+    'Werkzeug': {
+        '>=3.7': ['<3']  
     }
+
 }
 note = """"""
 
